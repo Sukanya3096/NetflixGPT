9094ef746ed06ddf1ad8594de9c5f8fb536dc908
Sukanya3096 <isukanyadutta@gmail.com>
2023-11-24 09:29:03 +0530

    browsing movies

---
+++ netflix-gpt/src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,22 @@
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+  const getNowPlayingMovies = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTIONS
+    );
+    const data = await response.json();
+    console.log(data);
+    dispatch(addNowPlayingMovies(data.results));
+  };
+};
+
+export default useNowPlayingMovies;
